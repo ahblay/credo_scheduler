77f1fac41c8b2d7b869ef743cedf54111460fd57
Abel Romer <abel.romer@questu.ca>
2019-01-23 15:29:13 -0800

    Updated models.py to add teachers table to database and remove test result table.

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from app import db
-from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy.dialects.postgresql import JSON, ARRAY
 
-
+'''
 class Result(db.Model):
     __tablename__ = 'results'
 
@@ -14,6 +14,23 @@ class Result(db.Model):
         self.url = url
         self.result_all = result_all
         self.result_no_stop_words = result_no_stop_words
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+'''
+
+
+class Teachers(db.Model):
+    __tablename__ = 'teachers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    classes = db.Column(ARRAY(db.String))
+
+    def __init__(self, id, name, classes):
+        self.id = id
+        self.name = name
+        self.classes = classes
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
