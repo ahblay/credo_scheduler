753d669f033208b57ee18c63a5c29e8481416414
Abel Romer <abel.romer@questu.ca>
2019-02-23 15:17:09 -0800

    Moved database actions to utilities.py.

--- app.py
+++ app.py
@@ -7,24 +7,14 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Teachers, Classes, ClassPrefs
-
-
-def to_boolean(string):
-    if string == "true":
-        return True
-    else:
-        return False
+from utilities import *
 
 
 @app.route('/add_teachers', methods=['GET', 'POST'])
 def add_teachers():
     teachers = []
     if request.method == "GET":
-        for teacher in Teachers.query.all():
-            if teacher.name:
-                data = [teacher.name, teacher.classes]
-                teachers.append(data)
+        teachers = get_teachers()
 
     if request.method == "POST":
         name = request.form['name']
@@ -33,35 +23,16 @@ def add_teachers():
         print(request.form['full_time'])
         print(name, classes, primary)
 
-        try:
-            to_update = Teachers.query.filter_by(name=name).first()
-            if not to_update:
-                result = Teachers(
-                    name=name,
-                    classes=classes,
-                    primary=primary
-                )
-                db.session.add(result)
-                db.session.commit()
-            else:
-                to_update.classes = classes
-                to_update.primary = primary
-                db.session.commit()
-        except Exception as e:
-            print(e)
-            print("Could not add to database")
+        update_teachers(db, name, classes, primary)
 
     return render_template('add_teachers.html', teachers=teachers)
 
 
 @app.route('/add_classes', methods=['GET', 'POST'])
 def add_classes():
-    courses = []
+    classes = []
     if request.method == 'GET':
-        classes = Classes.query.all()
-        for course in classes:
-            data = [course.name, course.subject, course.type, course.hours]
-            courses.append(data)
+        classes = get_classes()
 
     if request.method == 'POST':
         name = request.form['name']
@@ -69,42 +40,20 @@ def add_classes():
         type = request.form['type']
         hours = request.form['hours']
 
-        try:
-            to_update = Classes.query.filter_by(name=name).first()
-            if not to_update:
-                result = Classes(
-                    name=name,
-                    subject=subject,
-                    type=type,
-                    hours=hours
-                )
-                db.session.add(result)
-                db.session.commit()
-            else:
-                to_update.subject = subject
-                to_update.type = type
-                to_update.hours = hours
-                db.session.commit()
-        except Exception as e:
-            print(e)
-            print("Failed to update class.")
-    return render_template('add_classes.html', classes=courses)
+        update_classes(db, name, subject, type, hours)
+
+    return render_template('add_classes.html', classes=classes)
 
 
 @app.route('/prefs', methods=['GET', 'POST'])
 def enter_prefs():
-    class_prefs = {}
+    prefs = {}
+    classes = []
+    teachers = []
     if request.method == 'GET':
-        # get preference data from database
-        prefs = ClassPrefs.query.order_by(ClassPrefs.teacher_name).all()
-        for pref in prefs:
-            print(pref.teacher_name, pref.class_name, pref.pref)
-            if pref.teacher_name in class_prefs:
-                class_prefs[pref.teacher_name][pref.class_name] = pref.pref
-            else:
-                class_prefs[pref.teacher_name] = {}
-                class_prefs[pref.teacher_name][pref.class_name] = pref.pref
-        print(class_prefs)
+        prefs = get_prefs()
+        classes = get_classes()
+        teachers = get_teachers()
 
     if request.method == 'POST':
         # post updated preferences to database
@@ -114,26 +63,9 @@ def enter_prefs():
 
         print(teacher_name, class_name, pref)
 
-        try:
-            to_update = ClassPrefs.query.filter_by(teacher_name=teacher_name, class_name=class_name).first()
-            print(to_update)
-            if not to_update:
-                result = ClassPrefs(
-                    teacher_name=teacher_name,
-                    class_name=class_name,
-                    pref=pref
-                )
-                db.session.add(result)
-                db.session.commit()
-            else:
-                to_update.pref = pref
-                print("Updated database with pref data.")
-                db.session.commit()
-        except Exception as e:
-            print(e)
-            print("Could not update database.")
+        update_prefs(db, teacher_name, class_name, pref)
 
-    return render_template('prefs.html', prefs=class_prefs)
+    return render_template('prefs.html', prefs=prefs, classes=classes, teachers=teachers)
 
 
 @app.route("/delete", methods=["POST"])
---
+++ utilities.py
@@ -0,0 +1,99 @@
+from models import *
+
+
+def to_boolean(string):
+    if string == "true":
+        return True
+    else:
+        return False
+
+
+def get_teachers():
+    teachers = []
+    for teacher in Teachers.query.all():
+        if teacher.name:
+            data = [teacher.name, teacher.classes]
+            teachers.append(data)
+    return teachers
+
+
+def get_classes():
+    courses = []
+    for course in Classes.query.all():
+        data = [course.name, course.subject, course.type, course.hours]
+        courses.append(data)
+    return courses
+
+
+def get_prefs():
+    prefs = {}
+    for pref in ClassPrefs.query.order_by(ClassPrefs.teacher_name).all():
+        if pref.teacher_name in prefs:
+            prefs[pref.teacher_name][pref.class_name] = pref.pref
+        else:
+            prefs[pref.teacher_name] = {}
+            prefs[pref.teacher_name][pref.class_name] = pref.pref
+    return prefs
+
+
+def update_teachers(db, name, courses, primary):
+    try:
+        to_update = Teachers.query.filter_by(name=name).first()
+        if not to_update:
+            result = Teachers(
+                name=name,
+                classes=courses,
+                primary=primary
+            )
+            db.session.add(result)
+            db.session.commit()
+        else:
+            to_update.classes = courses
+            to_update.primary = primary
+            db.session.commit()
+    except Exception as e:
+        print(e)
+        print("Could not add to database")
+
+
+def update_classes(db, name, subject, type, hours):
+    try:
+        to_update = Classes.query.filter_by(name=name).first()
+        if not to_update:
+            result = Classes(
+                name=name,
+                subject=subject,
+                type=type,
+                hours=hours
+            )
+            db.session.add(result)
+            db.session.commit()
+        else:
+            to_update.subject = subject
+            to_update.type = type
+            to_update.hours = hours
+            db.session.commit()
+    except Exception as e:
+        print(e)
+        print("Failed to update class.")
+
+
+def update_prefs(db, teacher_name, class_name, pref):
+    try:
+        to_update = ClassPrefs.query.filter_by(teacher_name=teacher_name, class_name=class_name).first()
+        print(to_update)
+        if not to_update:
+            result = ClassPrefs(
+                teacher_name=teacher_name,
+                class_name=class_name,
+                pref=pref
+            )
+            db.session.add(result)
+            db.session.commit()
+        else:
+            to_update.pref = pref
+            print("Updated database with pref data.")
+            db.session.commit()
+    except Exception as e:
+        print(e)
+        print("Could not update database.")
